770689f4b412ca2d1e5895a3f0ccd12953ab915d
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-08-15 01:26:42 +0900

    Feat: Menu Entity 생성 #2

---
+++ src/main/java/com/KooKPaP/server/domain/menu/entity/Menu.java
@@ -0,0 +1,37 @@
+package com.KooKPaP.server.domain.menu.entity;
+
+import com.KooKPaP.server.domain.restaurant.entity.Restaurant;
+import com.KooKPaP.server.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "Member")
+@SQLDelete(sql = "UPDATE menu SET deleted_at = NOW() where id = ?") // SQL Delete 쿼리 시, 논리적 삭제로 바인딩되도록 하기 위한 용도
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 무분별한 생성자 사용을 막는 용도
+public class Menu extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, columnDefinition = "bigint")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false, columnDefinition = "int")
+    private Integer price;
+
+    @Column(name = "picture_url", columnDefinition = "text") // 이미지가 아닌, 이미지 주소를 저장하기에 네이밍 컨벤션 변경
+    private String pictureUrl;
+}
