3d9b921095df7e3c7dde729f563b1a2d5232d13b
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-18 01:53:21 +0900

    fix: Restaurant와 Operation 관계를 OneToOne으로 바꿈에 따른 쿼리 임시 수정 #20

--- src/main/java/com/KooKPaP/server/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/KooKPaP/server/domain/restaurant/repository/RestaurantRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     // 사용자가 Restaurant의 주인인지 확인하기 위한 코드
-    Boolean existsRestaurantByIdAndMemberIdAAndDeletedAtIsNull(Long id, Long memberId);
+    Boolean existsRestaurantByIdAndMemberIdAndDeletedAtIsNull(Long id, Long memberId);
 
     // 논리적 삭제가 안 된 Restaurant 정보 반환 코드
     Optional<Restaurant> findRestaurantByIdAndDeletedAtIsNull(Long id);
