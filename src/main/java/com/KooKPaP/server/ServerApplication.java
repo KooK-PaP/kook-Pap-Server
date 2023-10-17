c4f01a43aa5f37e0e269d0ba354b79ad4e1e0785
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-18 00:56:49 +0900

    style: ServerApplication에서 서버 실행되었음을 콘솔 로그로 확인할 수 있도록 추가 #20

--- src/main/java/com/KooKPaP/server/ServerApplication.java
+++ src/main/java/com/KooKPaP/server/ServerApplication.java
@@ -12,6 +12,7 @@ public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+		System.out.println("Server is running");
 	}
 
 }
