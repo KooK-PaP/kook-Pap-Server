eaea1f28e14165b954875ce2781ab251a670454a
RyoonHee Cho <jrh19072957@gmail.com>
2023-11-14 12:36:01 +0900

    refactor: getAuthorities 변경 #25

--- src/main/java/com/KooKPaP/server/ServerApplication.java
+++ src/main/java/com/KooKPaP/server/ServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ServerApplication {
 
 	public static void main(String[] args) {
